fd50c088dbdceb00d098bd2231de2b924f922c57
Allan Da Silva Pessin <allan.pessin@outlook.com>
2021-08-31 21:48:15 -0300

    criar novo card

--- script.js
+++ script.js
@@ -2,27 +2,28 @@
 const cards = document.querySelectorAll('.card')
 const dropZones = document.querySelectorAll('.dropzone')
 
-cards.forEach( card => {
+cards.forEach(card => {
   card.addEventListener('dragstart', dragstart)
   card.addEventListener('drag', drag)
   card.addEventListener('dragend', dragend)
 })
 
 function dragstart() {
-  dropZones.forEach( dropZone => dropZone.classList.add('highlight'))
+  dropZones.forEach(dropZone => dropZone.classList.add('highlight'))
   this.classList.add('is-dragging')
 }
 
+
 function drag() {
-  
+
 }
 
 function dragend() {
-  dropZones.forEach( dropZone => dropZone.classList.remove('highlight'))
+  dropZones.forEach(dropZone => dropZone.classList.remove('highlight'))
   this.classList.remove('is-dragging')
 }
 
-dropZones.forEach( dropZone  => {
+dropZones.forEach(dropZone => {
   dropZone.addEventListener('dragenter', dragenter)
   dropZone.addEventListener('dragover', dragover)
   dropZone.addEventListener('dragleave', dragleave)
@@ -40,9 +41,22 @@ function dragover() {
 }
 
 function dragleave() {
-  this.classList.remove('over')  
+  this.classList.remove('over')
 }
 
 function dragdrop() {
-  this.classList.remove('over')  
+  this.classList.remove('over')
 }
+
+
+document.getElementById('addCard').addEventListener('click', function () {
+  const div = document.createElement('div')
+  div.innerHTML = `
+    <div class="card" draggable="true">
+      <div class="status red"></div>
+      <div class="content" id="content">Novo card adicionado</div>
+    </div>`
+
+  element = document.querySelector('#dropZone1')
+  element.append(div, )
+}, false)
